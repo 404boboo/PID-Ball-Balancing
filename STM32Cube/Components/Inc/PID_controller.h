6d40fc7e4399e597cc8bcefd06418363eab5754b
yazidta <yazedtaa@gmail.com>
2024-01-22 17:45:51 +0100

    Merged sensor and servo
    optimized the sensor measurements
    merged the servo and sensor so that they both work on one project
    
    Co-Authored-By: Ahmed Bouras <100701180+404boboo@users.noreply.github.com>
    Co-Authored-By: Soplica52 <100694324+Soplica52@users.noreply.github.com>

---
+++ STM32Cube/Components/Inc/PID_controller.h
@@ -0,0 +1,13 @@
+/*
+ * PID_controller.h
+ *
+ *  Created on: Jan 22, 2024
+ *      Author: yazed almazroua
+ */
+
+#ifndef INC_PID_CONTROLLER_H_
+#define INC_PID_CONTROLLER_H_
+
+
+
+#endif /* INC_PID_CONTROLLER_H_ */
---
+++ STM32Cube/Components/Src/PID_controller.c
@@ -0,0 +1,8 @@
+/*
+ * PID_controller.c
+ *
+ *  Created on: Jan 22, 2024
+ *      Author: yazed almazroua
+ */
+
+
